2303f4bcd2094eeb6d0554bd4126d639a7af452e
jourdannf <jourdann.fraser@gmail.com>
2024-05-21 13:50:25 -0400

    Changed isOver25 variable and started part two

--- data_manipulation.js
+++ data_manipulation.js
@@ -1,3 +1,5 @@
+//PART ONE: Math Problems
+
 // The initial numbers that must be verified.
 const n1 = 10;
 const n2 = 15;
@@ -20,8 +22,6 @@ const isTwoOdd = (n1 % 2) + (n2 % 2) + (n3 % 2) + (n4 % 2) >= 2;
 // if ANY of the numbers is larger than 25.
 const isOver25 = n1 > 25 || n2 > 25 || n3 > 25 || n4 > 25;
 
-const isUnder25 = n1 < 25 || n2 < 25 || n3 < 25 || n4 < 25;
-
 // Check four: all unique numbers
 // This is long, and there are more efficient
 // ways of handling it with other data structures
@@ -41,7 +41,7 @@ const arithmeticChain = ((n3 * (n2 - n1))%n4);
 // for convenience. Note how we negate isOver25 using
 // the ! operator. We could also have tested for 
 // "isUnder25" as an alternative.
-const isValid = isSum50 && isTwoOdd && !isOver25 && isUnique && isDivisibleByFive && firstLargerThanLast;
+const isValid = isSum50 && isTwoOdd && isUnique || isOver25;
 
 // Finally, log the results.
 console.log(isValid);
@@ -56,3 +56,19 @@ const dontDoThis = ((n1 + n2 + n3 + n4) == 50) &&
   (n1 != n2 && n1 != n3 && n1 != n4 && n2 != n3 && n2 != n4 && n3 != n4);
 
   let x = 10;
+
+
+  //PART TWO: Practical Math
+
+  const totalDistance = 1500;
+  let fuelEfficiency = 55;
+
+  if (fuelEfficiency === 55){
+
+  }else if (fuelEfficiency === 60){
+
+  }else if (fuelEfficiency === 75){
+    
+  }else{
+    console.log("Your car cannot manuever at this rate");
+  }
