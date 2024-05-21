eb24204c1f925d79d66b3442060567dec80de131
jourdannf <jourdann.fraser@gmail.com>
2024-05-21 12:19:06 -0400

    Divisible by five check is complete

--- data_manipulation.js
+++ data_manipulation.js
@@ -26,11 +26,16 @@ const isOver25 = n1 > 25 || n2 > 25 || n3 > 25 || n4 > 25;
 // that we will review later.
 const isUnique = n1 != n2 && n1 != n3 && n1 != n4 && n2 != n3 && n2 != n4 && n3 != n4;
 
+//Check all numbers are divisible by 5
+const isDivisibleByFive = !(n1 % 5) && !(n2 % 5) && !(n3 % 5) && !(n4 % 5);
+
+
+
 // Here, we put the results into a single variable 
 // for convenience. Note how we negate isOver25 using
 // the ! operator. We could also have tested for 
 // "isUnder25" as an alternative.
-const isValid = isSum50 && isTwoOdd && !isOver25 && isUnique;
+const isValid = isSum50 && isTwoOdd && !isOver25 && isUnique && isDivisibleByFive;
 
 // Finally, log the results.
 console.log(isValid);
@@ -43,3 +48,5 @@ const dontDoThis = ((n1 + n2 + n3 + n4) == 50) &&
   ((n1 % 2) + (n2 % 2) + (n3 % 2) + (n4 % 2) >= 2) && 
   !(n1 > 25 || n2 > 25 || n3 > 25 || n4 > 25) && 
   (n1 != n2 && n1 != n3 && n1 != n4 && n2 != n3 && n2 != n4 && n3 != n4);
+
+  let x = 10;
